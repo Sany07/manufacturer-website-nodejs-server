fd1d98408c7276ccdebf0038b2bc565b6021ca42
Ahasan Habib Sany <sany.baust@gmail.com>
2022-04-29 17:40:51 +0600

    setup server

--- index.js
+++ index.js
@@ -11,19 +11,110 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vwx9p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@wire-h-web.iyul3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+ 
+async function run() {
+    try {
+        await client.connect();
+        const productsCollection = client.db('wh-db').collection('products');
+        // const orderCollection = client.db('geniusCar').collection('order');
 
+        // AUTH
+        // app.post('/login', async (req, res) => {
+        //     const user = req.body;
+        //     const accessToken = jwt.sign(user, process.env.JWT_SECRET, {
+        //         expiresIn: process.env.JwtExpiresIn
+        //     });
+        //     res.send({ accessToken });
+        // })
 
-// run().catch(console.dir);
+        // SERVICES API
+        // app.get('/service', async (req, res) => {
+        //     const query = {};
+        //     const cursor = serviceCollection.find(query);
+        //     const services = await cursor.toArray();
+        //     res.send(services);
+        // });
+
+        // app.get('/service/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     console.log(id);
+        //     const query = { _id: ObjectId(id) };
+        //     const service = await serviceCollection.findOne(query);
+        //     res.send(service);
+        // });
+
+        // POST
+        app.post('/product', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productsCollection.insertOne(newProduct);
+            res.send(result);
+        });
+
+        // // DELETE
+        // app.delete('/service/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const result = await serviceCollection.deleteOne(query);
+        //     res.send(result);
+        // });
+
+        // // Order Collection API
+
+        // app.get('/order', verifyJWT, async (req, res) => {
+        //     const decodedEmail = req.decoded.email;
+        //     const email = req.query.email;
+        //     if (email === decodedEmail) {
+        //         const query = { email: email };
+        //         const cursor = orderCollection.find(query);
+        //         const orders = await cursor.toArray();
+        //         res.send(orders);
+        //     }
+        //     else{
+        //         res.status(403).send({message: 'forbidden access'})
+        //     }
+        // })
+
+        // app.post('/order', async (req, res) => {
+        //     const order = req.body;
+        //     const result = await orderCollection.insertOne(order);
+        //     res.send(result);
+        // })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch('s',console.dir);
 
 app.get('/', (req, res) => {
     res.send('Running Genius Server');
 });
 
-app.get('/hero', (req, res) =>{
+app.get('/hi', (req, res) =>{
     res.send('Hero meets hero ku')
 })
 
